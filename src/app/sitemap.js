3f45a683055600eadae062c2191a903c6998a38b
Praveen Kumar <praveenkumarak02@gmail.com>
2026-04-13 20:42:41 +0530

    sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://www.framextechfarm.in/',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.framextechfarm.in/about',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.framextechfarm.in/services',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.framextechfarm.in/contact',
+      lastModified: new Date(),
+    },
+  ];
+}
